4c5055e86695a2361c762d9fc1a330214698b13d
IMH51 <ianmartinharrison@outlook.com>
2018-11-23 14:08:12 +0000

    created two sets of classes that use a has many through relationship to deliver information on objects

---
+++ lib/patient.rb
@@ -0,0 +1,28 @@
+class Patient
+
+  attr_accessor :doctor, :appointment, :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(doctor, date)
+    new_appointment = Appointment.new(doctor, self, date)
+  end
+
+  def appointments
+    Appointment.all.select {|appointment| appointment.patient == self}
+  end
+
+  def doctors
+    appointments.map {|appointment| appointment.doctor}
+  end
+
+end
